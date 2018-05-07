9a66677b0f04f6ac98228b7c0813c2238a687b8c
Alex Dunn <dunn.alex@gmail.com>
2018-05-07 11:27:00 -0700

    elfeed 2.3.0

--- Formula/elfeed.rb
+++ Formula/elfeed.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Elfeed < EmacsFormula
   desc "Feed reader for Emacs"
   homepage "https://github.com/skeeto/elfeed"
-  url "https://github.com/skeeto/elfeed/archive/2.2.0.tar.gz"
-  sha256 "8e7e4a26ce260cbaf9427ee5aa2c34812385f5b9b8b5d4541c938135e848f134"
+  url "https://github.com/skeeto/elfeed/archive/2.3.0.tar.gz"
+  sha256 "bbffa9fa3cf7ab78b57c55a34717222dfa985dbfb768fadbdb69e18041afa1b9"
   head "https://github.com/skeeto/elfeed.git"
 
   depends_on EmacsRequirement => "24.3"
